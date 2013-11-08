95aa3c319ccafb090ce4d76adfed6d8888f0b568
ochaochaocha3 <ochaochaocha3@gmail.com>
2013-11-08 12:05:44 +0000

    gitconfig.sh: Vim のエンコーディングを指定

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,11 +2,11 @@
 
 # デフォルトのユーザー名とメールアドレスの設定
 # FIXME: "Your xxx" を設定してコメント解除すること
-git config --global user.name "ochaochaocha3"
-git config --global user.email "ochaochaocha3@gmail.com"
+# git config --global user.name "Your name"
+# git config --global user.email "Your email"
 
 # Git で使用するエディタの設定
-git config --global core.editor vim
+git config --global core.editor 'vim -c "set fenc=utf-8"'
 
 # すべてのリポジトリで無視するパターンの登録
 git config --global core.excludesfile "~/.gitignore"
